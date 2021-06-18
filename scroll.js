9a5f9efdb29f9e015bc9011dbe7e402c67deec27
astralite7 <86109518+astralite7@users.noreply.github.com>
2021-06-18 21:49:40 +1000

    uploading files
    ...

---
+++ scroll.js
@@ -0,0 +1,6 @@
+$('.container').click(function(){
+    var id = $(this).attr('id');
+    var next = (Number(id) + 1);
+    //window.scrollTo(0, $('#'+next).offset().top);
+    $('html, body').animate({scrollTop : $('#'+next).offset().top}, 2000);
+  })
